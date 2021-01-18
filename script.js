a2aa599f8ccadd337b971bff8a32e392f49a7adb
lucifer2020812 <69583177+lucifer2020812@users.noreply.github.com>
2021-01-18 14:35:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+function Function() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "15px";
+  } else {
+    document.getElementById("header").style.fontSize = "25px";
+  }
+}
